ef54c883c5b00f21b9e1c6ca02170d01da284f08
ǝʞɔoʃʞ uɐʇsıɹʇ <communities@tristank.de>
2016-09-09 17:09:41 +0200

    remove persona

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -15,7 +15,6 @@ export default Ember.Controller.extend({
 
         you.set('peer.id', id);
         this.set('you', you);
-        this.handlePersonaAuth();
     },
 
     actions: {
@@ -36,44 +35,4 @@ export default Ember.Controller.extend({
         }
     },
 
-    handlePersonaAuth: function () {
-        var self = this;
-
-        navigator.id.watch({
-            loggedInUser: this.get('you.email'),
-
-            onlogin: function (assertion) {
-                Ember.$.ajax({
-                    type: 'POST',
-                    url: '/persona/verify',
-                    data: { assertion: assertion },
-                    success: function (res) {
-                        console.log('Persona: Signed in as: "' + res.email + '"');
-
-                        if (res && res.status === "okay") {
-                            self.set('you.email', res.email);
-                        }
-                    },
-                    error: function (xhr, status, err) {
-                        console.log('Persona: Signed in error: ', err);
-                        navigator.id.logout();
-                    }
-                });
-            },
-
-            onlogout: function () {
-                Ember.$.ajax({
-                    type: 'POST',
-                    url: '/persona/logout',
-                    success: function () {
-                        console.log('Persona: Signed out');
-                        self.set('you.email', null);
-                    },
-                    error: function (xhr, status, err) {
-                        console.log('Persona: Sign out error: ', err);
-                    }
-                });
-            }
-        });
-    }
 });
